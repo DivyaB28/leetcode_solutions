cf0941622f1a89b6e6a30ab0e36a1d3adf2905c3
DivyaB <divya.1si15ec029@gmail.com>
2024-06-26 09:19:51 -0400

    Adding linkedlist solutions using slow and fast pointers

---
+++ LinkedList/DesignLinkedList/DesignLinkedList.js
@@ -0,0 +1,115 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+var MyLinkedList = function () {
+  this.head = null;
+  this.tail = null;
+  this.length = 0;
+};
+
+/**
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function (index) {
+  if (index < 0 || index >= this.length) {
+    return -1;
+  }
+  let current = this.head;
+  for (let i = 0; i < index; i++) {
+    current = current.next;
+  }
+  return current.val;
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function (val) {
+  let newNode = new Node(val);
+  if (!this.head) {
+    this.head = newNode;
+    this.tail = newNode;
+  } else {
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+  this.length++;
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function (val) {
+  const newNode = new Node(val);
+  if (!this.tail) {
+    this.head = newNode;
+    this.tail = newNode;
+  } else {
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+  this.length++;
+};
+
+/**
+ * @param {number} index
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function (index, val) {
+  if (index < 0 || index > this.length) {
+    return -1;
+  }
+  if (index === 0) {
+    this.addAtHead(val);
+  } else if (index === this.length) {
+    this.addAtTail(val);
+  } else {
+    let newNode = new Node(val);
+    let temp = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      temp = temp.next;
+    }
+    newNode.next = temp.next;
+    temp.next = newNode;
+    this.length++;
+  }
+};
+
+/**
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function (index) {
+  if (index < 0 || index >= this.length) return -1;
+  if (index === 0) {
+    this.head = this.head.next;
+  } else {
+    let before = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      before = before.next;
+    }
+    before.next = before.next.next;
+    if (index === this.length - 1) {
+      this.tail = before;
+    }
+  }
+
+  this.length--;
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
